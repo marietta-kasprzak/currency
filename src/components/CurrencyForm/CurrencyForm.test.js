9eeb398ab88e364e627303394eebd28ef5d0391c
Marietta Kasprzak <marietta.kasprzak@gmail.com>
2022-12-20 18:48:07 +0100

    currency

--- src/components/CurrencyForm/CurrencyForm.test.js
+++ src/components/CurrencyForm/CurrencyForm.test.js
@@ -1,17 +1,48 @@
+import { render, screen, cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import CurrencyForm from './CurrencyForm';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event'
 
+describe('Component CurrencyForm', () => {
+  it('should render without crashing', () => {
+    render(<CurrencyForm action={() => {}} />);
+  });
 
-describe ('component CurrencyForm', () => {
-    it ('should render without crashing', ()=> {
-        render(<CurrencyForm action={() => {}}/> );
-    })
-    it ('should run action callback with proper data on form submit', () => {
-        const action = jest.fn();
-        render(<CurrencyForm action={action}/>);
-        const submitButton = screen.getByText('Convert');
-        userEvent.click(submitButton);
-        expect(action).toHaveBeenCalledTimes(1);
-    })
-})
+  const testCases = [
+    { amount: '100', from: 'PLN', to: 'USD' },
+    { amount: '20', from: 'USD', to: 'PLN' },
+    { amount: '200', from: 'PLN', to: 'USD' },
+    { amount: '345', from: 'USD', to: 'PLN' },
+  ];
+
+  for (const testObj of testCases) {
+
+    it('should run action callback with proper data on form submit', () => {
+      const action = jest.fn();
+  
+      // render component
+      render(<CurrencyForm action={action} />);
+  
+      // find "convert" button
+      const submitButton = screen.getByText('Convert');
+  
+      // find field elems
+      const amountField = screen.getByTestId('amount');
+      const fromField = screen.getByTestId('from-select');
+      const toField = screen.getByTestId('to-select');
+  
+      // set test values to fields
+      userEvent.type(amountField, testObj.amount);
+      userEvent.selectOptions(fromField, testObj.from);
+      userEvent.selectOptions(toField, testObj.to);
+  
+      // simulate user click on "convert" button
+      userEvent.click(submitButton);
+  
+      // check if action callback was called once and with proper argument 
+      expect(action).toHaveBeenCalledTimes(1);
+      expect(action).toHaveBeenCalledWith({ amount: parseInt(testObj.amount), from: testObj.from, to: testObj.to });
+      // unmount component
+      cleanup()
+    });
+  }
+});
